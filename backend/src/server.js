09a250dd4412918441fdd6d6e8738423f978e7d1
Francisco <quicco.sousa@gmail.com>
2025-10-09 16:35:09 +0100

    Fix DB connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,10 @@
-import express from "express"
-import cors from "cors";
-import dotenv from "dotenv"
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-import notesRoutes from "./routes/notesRoutes.js";
-import gamesRoutes from "./routes/gamesRoutes.js"
-import { connectDB } from "./config/db.js";
-
+import notesRoutes from './routes/notesRoutes.js';
+import gamesRoutes from './routes/gamesRoutes.js';
+import { connectDB } from './config/db.js';
 
 dotenv.config();
 
@@ -17,12 +16,12 @@ app.use(express.json());
 app.use(cors());
 
 // tutorial related code
-app.use("/api/notes", notesRoutes);
+app.use('/api/notes', notesRoutes);
 // nuzlocke related code
-app.use("/games", gamesRoutes);
+app.use('/games', gamesRoutes);
 
 connectDB().then(() => {
-    app.listen(port, () => {
-        console.log(`Server started on PORT: ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Server started on PORT: ${port}`);
+  });
 });
